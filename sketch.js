e793889773405b90d055cff58d14ff79915bf215
Stephen Buchanan <stephen@mannmademedia.com>
2016-09-08 13:48:57 +0200

    can find random starting point in grid for new Circuit

--- sketch.js
+++ sketch.js
@@ -2,6 +2,8 @@ var GRID_PIXELS = 10;
 var grid = [];
 var gridWidth, gridHeight;
 
+var circuits = [];
+
 function setup() {
   createCanvas(800, 800);
 
@@ -13,11 +15,13 @@ function setup() {
   for (x = 0; x < gridWidth; x++) {
     grid[x] = [];
     for (y = 0; y < gridHeight; y++) {
-      grid[x][y] = 'empty';
+      grid[x][y] = 'open';
     }
   }
 
   console.log('grid:', grid);
+
+  circuits[0] = new Circuit();
 
 }
 
@@ -29,22 +33,14 @@ function draw() {
     for (y = 0; y < gridHeight; y++) {
       var cell = grid[x][y];
 
-      if (cell == 'empty') {
-        // console.log('empty at', x, y);
-        fill(100);
-        text('e', gridToPixels(x), gridToPixels(y));
-      }
-      if (cell == 'line') {
-        // console.log('empty at', x, y);
-        fill(100);
-        text('l', gridToPixels(x), gridToPixels(y));
-      }
-      if (cell == 'hole') {
-        // console.log('empty at', x, y);
+      if (cell == 'open') {
         fill(100);
         text('o', gridToPixels(x), gridToPixels(y));
       }
-
+      if (cell == 'used') {
+        fill(100);
+        text('x', gridToPixels(x), gridToPixels(y));
+      }
 
     }
   }
@@ -53,4 +49,48 @@ function draw() {
 
 function gridToPixels(input) {
   return input * GRID_PIXELS;
+}
+
+
+function randomCellCoords() {
+  return {
+    x: Math.floor(random(0, gridWidth)),
+    y: Math.floor(random(0, gridHeight))
+  };
+}
+
+function findEmptyCellCoords() {
+  var found = false;
+  var tries = 0;
+  var MAX_TRIES = 10;
+
+  var cell = 'nothing';
+  var coords;
+
+  while (cell != 'open' && tries < MAX_TRIES) {
+    tries++;
+    coords = randomCellCoords();
+    console.log(coords.x, coords.y);
+    cell = grid[coords.x][coords.y];
+    // console.log('searching', cell);
+  }
+  if (cell == 'open') {
+    return {
+      x: coords.x,
+      y: coords.y
+    }
+  } else {
+    return null;
+  }
+}
+
+// Circuit Class
+function Circuit() {
+  this.path = [];
+  this.startPosition = findEmptyCellCoords();
+  console.log('Circuit start:', this.startPosition);
+}
+
+Circuit.prototype.draw = function(){
+
 }
